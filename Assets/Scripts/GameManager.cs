c5dd559e381e75b3c6fe9e9d7bbda52a14bf1422
Alaa Eltayeb <eltayebalaa@gmail.com>
2025-02-26 23:52:47 +0100

    Remove PlayerController From GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,7 +3,6 @@ using Assets.Scripts.BoardGeneration.BoardPatterns;
 using Assets.Scripts.BoardGeneration.Tiles;
 using Assets.Scripts.Command;
 using Assets.Scripts.Quiz;
-using DG.Tweening;
 using System.Collections.Generic;
 using UnityEngine;
 using VContainer;
@@ -13,15 +12,6 @@ namespace Assets.Scripts
     public sealed class GameManager : MonoBehaviour
     {
         private IBoardGenerator _generator;
-
-        [SerializeField]
-        private Transform _playerController;
-        [SerializeField]
-        private Animator _playerAnimator;
-        [SerializeField]
-        private ParticleSystem _particleSystem;
-        [SerializeField]
-        private Animator _floatingTextAnimator;
 
         [SerializeField]
         private int _radius = 5;
@@ -36,16 +26,10 @@ namespace Assets.Scripts
         [SerializeField]
         private BoardPatternType _patternType;
 
-        private int _playerCurrentPositionIndex;
-        private int _playerTargetPositionIndex;
         private List<ITile> _tiles;
-
-        private float _moveCooldown = 1;
 
         private const int MinDiceCount = 1;
         private const int MaxDiceCount = 7;
-
-        private bool _isMoving;
 
         [SerializeField]
         private List<TextAsset> _quizTextAssets;
@@ -85,53 +69,13 @@ namespace Assets.Scripts
             }
 
             _commandDispatcher.Execute(
-                new SetInitialPlayerPositionCommand(_tiles[_playerCurrentPositionIndex].Position));
-            //_playerController.position = _tiles[_playerCurrentPositionIndex].Position;
+                new SetInitialPlayerPositionCommand(_tiles[0].Position));
         }
 
         public void PrepareMove()
         {
-            if (_isMoving)
-                return;
-
             var stepsCount = Random.Range(MinDiceCount, MaxDiceCount);
             _commandDispatcher.Execute(new MoveCommand(stepsCount, _tiles));
-            Debug.Log(stepsCount);
-            _playerTargetPositionIndex = _playerCurrentPositionIndex;
-            _playerTargetPositionIndex += stepsCount;
-
-            _isMoving = true;
-            MovePlayer();
-        }
-
-        private void MovePlayer()
-        {
-            if (_playerCurrentPositionIndex >= _playerTargetPositionIndex)
-            {
-                _isMoving = false;
-                return;
-            }
-
-            if (_tiles[_playerCurrentPositionIndex] is EmptyTile)
-            {
-                _particleSystem.Play();
-                _floatingTextAnimator.SetTrigger("Play");
-            }
-
-            _playerCurrentPositionIndex++;
-            if (_playerCurrentPositionIndex >= _tiles.Count)
-            {
-                _playerCurrentPositionIndex = 0;
-                _playerTargetPositionIndex -= _tiles.Count;
-            }
-
-            _playerAnimator.SetTrigger("Jump");
-
-            var target = _tiles[_playerCurrentPositionIndex].Position;
-            _playerController
-                .DOMove(target, _moveCooldown)
-                .SetEase(Ease.InOutSine)
-                .onComplete = MovePlayer;
         }
     }
 }
